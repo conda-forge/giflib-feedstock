cffa7e3827acc1dc1f9d8565ef86c679b5855f63
Ali Khodabakhsh <ali.khodabakhsh@ntnu.no>
2018-01-26 17:23:57 +0100

    RLS path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ cd %SRC_DIR%
 set SLN_FILE=giflib.sln
 set SLN_CFG=Release
 if "%ARCH%"=="32" (set SLN_PLAT=x86) else (set SLN_PLAT=x64)
+if "%ARCH%"=="32" (set RLS_PLAT=%SRC_DIR%\vs2015\Release) else (set RLS_PLAT=%SRC_DIR%\vs2015\%SLN_PLAT%\Release)
 set PATH=%PATH%;C:\windows\Microsoft.NET\Framework\v4.0
 
 rem wget http://www.chemformatter.com/build-with-visual-studio-2015/giflib/giflib-5.1.4-vs14.zip
@@ -14,7 +15,7 @@ REM Build step
 MSBuild %SLN_FILE% /p:Configuration=Release,Platform=%SLN_PLAT%
 if errorlevel 1 exit 1
 
-copy %SRC_DIR%\vs2015\%SLN_PLAT%\Release\*.dll %LIBRARY_BIN%\
-copy %SRC_DIR%\vs2015\%SLN_PLAT%\Release\*.exe %LIBRARY_BIN%\
-copy %SRC_DIR%\vs2015\%SLN_PLAT%\Release\*.lib %LIBRARY_LIB%\
+copy %RLS_PLAT%\*.dll %LIBRARY_BIN%\
+copy %RLS_PLAT%\*.exe %LIBRARY_BIN%\
+copy %RLS_PLAT%\*.lib %LIBRARY_LIB%\
 REM copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.lib %LIBRARY_LIB%\
