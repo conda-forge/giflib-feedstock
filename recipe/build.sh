4e5a6807a880d64e534799b5fca51a2eb4f1afc7
Isuru Fernando <isuruf@gmail.com>
2019-11-05 21:13:52 -0600

    Run tests only on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j$CPU_COUNT
-make -C ../tests UTILS=$PWD
+if [[ "$target_platform" == linux* ]]; then
+  make -C ../tests UTILS=$PWD
+fi
 make install
